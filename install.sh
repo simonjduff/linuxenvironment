5974ca581c64bcc9aa075a82df3190ac5e6f0009
Simon Duff <simon.j.duff@gmail.com>
2022-03-02 13:40:54 +0000

    Environment variables scripts

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
+#!/bin/bash
 if [ -a ~/.profile ]
 	then
 		mv ~/.profile ~/.profile1
 fi
-ln -s -r scripts/.profile ~/.profile
+ln -sfr ./scripts/.profile ~/.profile
 
 if [ -a ~/.bashrc ]
 	then
 		mv ~/.bashrc ~/.bashrc1
 fi
-ln -s -r scripts/.bashrc ~/.bashrc
+ln -sfr ./scripts/.bashrc ~/.bashrc
 
 if [ -a ~/.bash_aliases ]
 	then
 		mv ~/.bash_aliases ~/.bash_aliases1
 fi
-ln -sr scripts/.bash_aliases ~/.bash_aliases
-ln -sfr scripts/.git_prompt.sh ~/.git_prompt.sh
+ln -sfr ./scripts/.bash_aliases ~/.bash_aliases
+ln -sfr ./scripts/.git_prompt.sh ~/.git_prompt.sh
